29f2a93f1f3fe79ba6719797a80a41820b965987
Mohamedthameemau5 <mdthameem1993@gmail.com>
2022-11-26 20:30:10 +0530

    Challenge 18

---
+++ JavaScript DOM Challenges/Challenge18.js
@@ -0,0 +1,9 @@
+var allPara = document.querySelectorAll('p');
+for(i=0;i<allPara.length;i++){
+    allPara[i].style.color = "green"
+}
+
+// var allPara = document.getElementsByTagName('p');
+// for(var eachP of allPara){
+//     eachP.style.color = "green"
+// }
