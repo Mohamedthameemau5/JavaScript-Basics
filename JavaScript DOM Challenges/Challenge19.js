ba685c45437a94b28bab9f656faa60ec78613a1f
Mohamedthameemau5 <mdthameem1993@gmail.com>
2022-11-26 20:39:56 +0530

    Challenge 19 commited

---
+++ JavaScript DOM Challenges/Challenge19.js
@@ -0,0 +1,6 @@
+var heading = document.querySelector('h1');       
+heading.style.color = "red";    // in to the header 
+
+
+var date = document.getElementsByClassName('date');     
+date[1].className = "red";       // to the date in the second article                   
