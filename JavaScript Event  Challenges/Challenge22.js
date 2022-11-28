9b587406d564a0744039d7938c937ef5daf423c7
Mohamedthameemau5 <mdthameem1993@gmail.com>
2022-11-28 13:33:35 +0530

    Challenge 22 commited

---
+++ JavaScript Event  Challenges/Challenge22.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('button');
+var para = document.querySelector('p');
+
+// btn.onclick = function(){
+//     para.style.color= "green"
+// }
+
+btn.addEventListener('click',function(){
+    para.style.color= "green"
+})
