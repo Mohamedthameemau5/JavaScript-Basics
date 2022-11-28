9316ab8e30f5abde19f1650db0f7c73483a2f7f6
Mohamedthameemau5 <mdthameem1993@gmail.com>
2022-11-28 18:42:15 +0530

    challenge 30 submitted

---
+++ JavaScript Event  Challenges/Challenge30.js
@@ -0,0 +1,16 @@
+var bodyTag = document.querySelector('body');
+
+document.addEventListener('keydown',function(event){
+
+    var keyPressed = event.which;
+
+    switch(keyPressed){
+        case 66: bodyTag.className="one";break;
+        case 86: bodyTag.className="two";break;
+        case 67: bodyTag.className="three";break;
+        case 70: bodyTag.className="four";break;
+        case 71: bodyTag.className="five";break;
+        default: alert("You didn't press one of the specified keys!");  
+
+    }
+});
